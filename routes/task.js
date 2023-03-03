94d70dcdbb64d91e32ecfe43736657d2a16fbd0c
ayush <ayush@osmosys.co>
2023-03-04 00:52:42 +0530

    delete task by id

--- routes/task.js
+++ routes/task.js
@@ -1,3 +1,4 @@
+const deleteTask = require("../controller/task/deleteTask");
 const getAllTask = require("../controller/task/getAllTask");
 const getTaskById = require("../controller/task/getTaskById");
 const postTask = require("../controller/task/postTask");
@@ -9,5 +10,6 @@ router.get("/task", getAllTask);
 router.post("/task", postTask);
 router.get("/task/:task_id", getTaskById);
 router.patch("/task/:task_id", updateTask);
+router.delete("/task/:task_id", deleteTask);
 
 module.exports = router;
