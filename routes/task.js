063951faa77f6704eb0c77b990ff13d54a78b75f
ayush <ayush@osmosys.co>
2023-03-04 00:48:10 +0530

    add update task by id

--- routes/task.js
+++ routes/task.js
@@ -1,11 +1,13 @@
 const getAllTask = require("../controller/task/getAllTask");
 const getTaskById = require("../controller/task/getTaskById");
 const postTask = require("../controller/task/postTask");
+const updateTask = require("../controller/task/updateTask");
 
 const router = require("express").Router();
 
 router.get("/task", getAllTask);
 router.post("/task", postTask);
 router.get("/task/:task_id", getTaskById);
+router.patch("/task/:task_id", updateTask);
 
 module.exports = router;
